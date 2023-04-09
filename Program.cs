96d073e8b8f6b071719162b25733f83dd917ba17
SergeyRomano <127023352+SergeyRomano@users.noreply.github.com>
2023-04-09 17:13:56 +0700

    Создал метод, который добавляет строки длиной не менее 3 во 2 массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-string[] array1 = new string[5] {"Car", "Dog", "hello", "world", "restourant"};
-string[] array2 = new string[array1.Length];
+string[] a1 = new string[5] {"Car", "Dog", "hello", "world", "restourant"};
+string[] a2 = new string[a1.Length];
+
+void SecondArray(string[] a1, string[] a2)
+{
+    int count = 0;
+    for (int i = 0; i < a1.Length; i++)
+    {
+    if(a1[i].Length <= 3)
+        {
+        a2[count] = a1[i];
+        count++;
+        }
+    }
+}
